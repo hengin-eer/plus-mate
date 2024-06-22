f8868feb0f294aade4647c02f9feea71f2b6a8ea
hengin-eer <hirokix0608@gmail.com>
2024-06-23 00:40:17 +0900

    ✨ feat: `IconButton`を利用した子コンポーネントの作成

---
+++ src/routes/CreateEventButton.svelte
@@ -0,0 +1,8 @@
+<script>
+	import IconButton from "./IconButton.svelte";
+</script>
+
+<IconButton
+    iconName="material-symbols-light:add-circle-outline-rounded"
+    actionTitle="イベントを作成する"
+/>
---
+++ src/routes/LoginButton.svelte
@@ -0,0 +1,11 @@
+<script>
+	import IconButton from "./IconButton.svelte";
+    const alertWithLogin = () => alert("Yes! Login!");
+</script>
+
+<IconButton
+    iconName="material-symbols-light:login-outline-rounded"
+    actionTitle="ログインする"
+    actionMethod={alertWithLogin}
+    --background="#41F494"
+/>
